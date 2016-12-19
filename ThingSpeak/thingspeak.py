2fe49b4e10b039c840b6ae9ab7dca05c3d73bd69
Leonardo <leonardo.ribeiro@gee.inatel.br>
2016-12-19 15:24:38 -0200

    Update thingspeak.py

--- ThingSpeak/thingspeak.py
+++ ThingSpeak/thingspeak.py
@@ -22,7 +22,7 @@ def process_data(raw):
 		print raw[0]
 		try:
 			url = 'https://api.thingspeak.com/update?api_key=WGAESYN974EPF93V&field1=%s' % (raw[0])
-    		r=requests.get(url)
+    			r=requests.get(url)
 			print 'sent to thingspeak'
 		except:
 			print 'problem in thingspeak'
